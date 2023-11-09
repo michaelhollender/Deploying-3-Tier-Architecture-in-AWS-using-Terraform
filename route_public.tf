0fe37d9d7a74f26aac8cc075de820ce5b48373e6
mastersplinter2 <111327102+michaelhollender@users.noreply.github.com>
2023-11-09 14:34:01 -0500

    Created Original Pubic Route

---
+++ route_public.tf
@@ -0,0 +1,27 @@
+#################
+## Route Table ##
+#################
+
+
+resource "aws_route_table" "public-route-table" {
+    vpc_id = aws_vpc.vpc_01.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id
+    }
+
+    tags = {
+        Name = "Public Route Table"
+    }
+}
+
+#############################
+## Route Table Association ##
+#############################
+
+
+resource "aws_route_table_association" "3-tier-web-1a-public-subnet-1-route-table-association" {
+    subnet_id      = aws_subnet.public-subnet-1.id
+    route_table_id = aws_route_table.public-route-table.id
+}
