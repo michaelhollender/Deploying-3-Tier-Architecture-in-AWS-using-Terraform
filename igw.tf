99dd777c5c141817453adc9006d0f8385aad5823
mastersplinter2 <111327102+michaelhollender@users.noreply.github.com>
2023-11-09 13:36:20 -0500

    Created Origial Internet Gateway

---
+++ igw.tf
@@ -0,0 +1,12 @@
+###############
+##### IGW #####
+###############
+
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.vpc_01.id 
+
+    tags = {
+        Name = "3-Tier IGW"
+    }
+}
