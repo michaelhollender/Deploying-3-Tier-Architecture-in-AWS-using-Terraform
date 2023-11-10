e32c4c65a618fbf77dfae4d3a45a4e3a56b43d48
mastersplinter2 <111327102+michaelhollender@users.noreply.github.com>
2023-11-10 12:40:23 -0500

    Fix Typos

--- subnets.tf
+++ subnets.tf
@@ -8,12 +8,12 @@ resource "aws_subnet" "public-subnet-1" {
     availability_zone        = "us-east-1a"
     map_public_ip_on_launch  = true
 
-  tags = {
+  tags = { 
     Name = "3-Tier-WEB-1A Public Subnet 1"
   }
 } 
   
-
+ 
 ###################################
 ## 3-Tier-WEB-1B Public Subnet 2 ##
 ###################################
@@ -29,7 +29,7 @@ resource "aws_subnet" "public-subnet-2" {
     }
 }
 
-
+ 
 ###################################
 ## 3-Tier-WEB-1C Public Subnet 3 ##
 ###################################
@@ -44,7 +44,7 @@ resource "aws_subnet" "public-subnet-3" {
         Name = "3-Tier-WEB-1C Public Subnet 3"
     }
 }
-
+ 
 
 ####################################
 ## 3-Tier-APP-1A Private Subnet 1 ##
@@ -70,8 +70,8 @@ resource "aws_subnet" "private-subnet-2" {
     vpc_id                  = aws_vpc.vpc_01.id
     cidr_block              = var.private-subnet-2-cidr
     availability_zone       = "us-east-1b"
-    map_public_ip_on_launch = false
-
+    map_public_ip_on_launch = false 
+ 
     tags = {
         Name = "3-Tier-APP-1B Private Subnet 2"
     }
@@ -87,26 +87,26 @@ resource "aws_subnet" "private-subnet-3" {
     cidr_block              = var.private-subnet-3-cidr
     availability_zone       = "us-east-1c"
     map_public_ip_on_launch = false
-
+  
     tags = {
         Name = "3-Tier-APP-1C Private Subnet 3"
     }
 } 
 
-
+   
 ####################################
-## 3-Tier-DB-1A Private Subnet 4 ##
+## 3-Tier-DB-1A Private Subnet 4 ## 
 ####################################
 
-resource "aws_subnet" "private-subnet-4" {
-    vpc_id                  = aws_vpc.vpc_01,id
+resource "aws_subnet" "private-subnet-4"  {
+    vpc_id                  = aws_vpc.vpc_01.id
     cidr_block              = var.private-subnet-4-cidr
     availability_zone       = "us-east-1a"
-    map_public_ip_on_launch = false 
-
+    map_public_ip_on_launch = false  
+ 
     tags = {
         Name = "3-Tier-DB-1A Private Subnet 4"
-    }
+     }
 }
 
 
@@ -126,7 +126,7 @@ resource "aws_subnet" "private-subnet-5" {
 }
 
 
-###################################
+################################## #
 ## 3-Tier-DB-1C Private Subnet 6 ##
 ###################################
 
