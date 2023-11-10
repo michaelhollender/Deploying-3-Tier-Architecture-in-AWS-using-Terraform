d9447f5da94b1a4527460b3d0d6b1639eddea59c
mastersplinter2 <111327102+michaelhollender@users.noreply.github.com>
2023-11-10 18:23:29 -0500

    Created Original Launch Templates for Web and App

---
+++ ec2.tf
@@ -0,0 +1,33 @@
+###########################
+## EC2 Instance Web Tier ##
+###########################
+
+resource "aws_instance" "3TierWebLaunchTemplate" {
+    ami                    = "ami-"
+    instance_type          = "t2.micro"
+    subnet_id              = aws_subnet.public-subnet-1.id
+    vpc_security_group_ids = [aws_security_group.webserver-security-group.id]
+    key_name               = "3_tier_rsa_key"
+    user_data              = file("install-apache.sh")
+
+    tags = {
+        Name = "web-asg"
+    }
+}
+
+
+###########################
+## EC2 Instance App Tier ##
+###########################
+
+resource "aws_instance" "3TierAppLaunchTemplate" {
+    ami                    = "ami-"
+    install_type           = "t2.micro"
+    subnet_id              = aws_subnet.private-subnet-1.id
+    vpc_security_group_ids = [aws_security_group.ssh-security-group.id]
+    key_name               = "3_tier_rsa_key"
+
+    tags = {
+        Name = "app-asg"
+    }
+}
