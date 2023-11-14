80945a54d7291ea1363c60771459af70d2a1ba81
mastersplinter2 <111327102+michaelhollender@users.noreply.github.com>
2023-11-14 00:12:13 -0500

    Fixed Format

--- app_sg.tf
+++ app_sg.tf
@@ -3,33 +3,33 @@
 ########################################
 
 resource "aws_security_group" "ssh-security-group" {
-    name        = "SSH Access"
-    description = "Enable SSH access on port 22"
-    vpc_id = aws_vpc.vpc_01.id
+  name        = "SSH Access"
+  description = "Enable SSH access on port 22"
+  vpc_id      = aws_vpc.vpc_01.id
 
-    ingress {
-        description = "ssh access"
-        from_port   = 22
-        to_port     = 22
-        protocol    = "tcp"
-        cidr_blocks = ["${var.ssh-locate}"]
-    }
-    
-    ingress {
-        description = "ping access"
-        from_port   = 8
-        to_port     = 0
-        protocol    = "icmp"
-    }
+  ingress {
+    description = "ssh access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.ssh-locate}"]
+  }
 
-    egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = -1
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    description = "ping access"
+    from_port   = 8
+    to_port     = 0
+    protocol    = "icmp"
+  }
 
-    tags = {
-        Name = "SSH Security Group"
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "SSH Security Group"
+  }
 }
