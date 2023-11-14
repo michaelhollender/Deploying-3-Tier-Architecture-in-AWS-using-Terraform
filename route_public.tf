2ba5097192d6226e52d1151ecfb85d1787601220
mastersplinter2 <111327102+michaelhollender@users.noreply.github.com>
2023-11-14 00:13:04 -0500

    Fixed Format

--- route_public.tf
+++ route_public.tf
@@ -4,16 +4,16 @@
 
 
 resource "aws_route_table" "public-route-table" {
-    vpc_id = aws_vpc.vpc_01.id
+  vpc_id = aws_vpc.vpc_01.id
 
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.igw.id
-    }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
 
-    tags = {
-        Name = "Public Route Table"
-    }
+  tags = {
+    Name = "Public Route Table"
+  }
 }
 
 #############################
@@ -22,6 +22,6 @@ resource "aws_route_table" "public-route-table" {
 
 
 resource "aws_route_table_association" "Three-tier-web-1a-public-subnet-1-route-table-association" {
-    subnet_id      = aws_subnet.public-subnet-1.id
-    route_table_id = aws_route_table.public-route-table.id
+  subnet_id      = aws_subnet.public-subnet-1.id
+  route_table_id = aws_route_table.public-route-table.id
 }
