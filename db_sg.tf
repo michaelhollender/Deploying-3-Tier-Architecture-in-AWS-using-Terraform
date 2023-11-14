2a7e15ab1e5ca4f77bec367259089ec0f63af426
mastersplinter2 <111327102+michaelhollender@users.noreply.github.com>
2023-11-14 00:12:25 -0500

    Fixed Format

--- db_sg.tf
+++ db_sg.tf
@@ -3,26 +3,26 @@
 ######################
 
 resource "aws_security_group" "database-security-group" {
-    name        = "Database Server Security Group"
-    description = "Enable MYSQL Access on Port 3306"
-    vpc_id = aws_vpc.vpc_01.id 
+  name        = "Database Server Security Group"
+  description = "Enable MYSQL Access on Port 3306"
+  vpc_id      = aws_vpc.vpc_01.id
 
-    ingress {
-        description = "MYSQL Access"
-        from_port   = "3306"
-        to_port     = "3306"
-        protocol    = "tcp"
-        security_group = ["{aws_security_group.webserver-security-group.id}"]
-    }
+  ingress {
+    description    = "MYSQL Access"
+    from_port      = "3306"
+    to_port        = "3306"
+    protocol       = "tcp"
+    security_group = ["{aws_security_group.webserver-security-group.id}"]
+  }
 
-    egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    tags = {
-        Name = "Database Security Group"
-    }
+  tags = {
+    Name = "Database Security Group"
+  }
 }
