da56224269b4d026f0b49685eb891121faccdb49
mastersplinter2 <111327102+michaelhollender@users.noreply.github.com>
2023-11-14 00:12:58 -0500

    Fixed Format

--- route_private.tf
+++ route_private.tf
@@ -3,16 +3,16 @@
 #########################
 
 resource "aws_route_table" "private-route-table" {
-    vpc_id = aws_vpc.vpc_01.id
+  vpc_id = aws_vpc.vpc_01.id
 
-    route {
-        cidr_block     = "0.0.0.0/0"
-        nat_gateway_id = aws_nat_gateway.Three_Tier_NAT.vpc_id
-    }
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.Three_Tier_NAT.vpc_id
+  }
 
-    tags = {
-        Name = "3-Tier-Private-Route-Table"
-    }
+  tags = {
+    Name = "3-Tier-Private-Route-Table"
+  }
 }
 
 
@@ -21,18 +21,18 @@ resource "aws_route_table" "private-route-table" {
 ####################################
 
 resource "aws_route_table_association" "nat_route_app_1" {
-    subnet_id      = aws_subnet.private-subnet-1.id
-    route_table_id = aws_route_table.private-route-table.id
+  subnet_id      = aws_subnet.private-subnet-1.id
+  route_table_id = aws_route_table.private-route-table.id
 }
 
 resource "aws_route_table_association" "nat_route_app_2" {
-    subnet_id      = aws_subnet.private-subnet-2.id
-    route_table_id = aws_route_table.private-route-table.id
+  subnet_id      = aws_subnet.private-subnet-2.id
+  route_table_id = aws_route_table.private-route-table.id
 }
 
 resource "aws_route_table_association" "nat_route_app_3" {
-    subnet_id      = aws_subnet.private-subnet-3.id
-    route_table_id = aws_route_table.private-route-table.id
+  subnet_id      = aws_subnet.private-subnet-3.id
+  route_table_id = aws_route_table.private-route-table.id
 }
 
 
@@ -41,16 +41,16 @@ resource "aws_route_table_association" "nat_route_app_3" {
 ###################################
 
 resource "aws_route_table_association" "nat_route_db_1" {
-    subnet_id      = aws_subnet.private-subnet-4.id
-    route_table_id = aws_route_table.private-route-table.id
+  subnet_id      = aws_subnet.private-subnet-4.id
+  route_table_id = aws_route_table.private-route-table.id
 }
 
 resource "aws_route_table_association" "nat_route_db_2" {
-    subnet_id      = aws_subnet.private-subnet-5.id
-    route_table_id = aws_route_table.private-route-table.id
+  subnet_id      = aws_subnet.private-subnet-5.id
+  route_table_id = aws_route_table.private-route-table.id
 }
 
 resource "aws_route_table_association" "nat_route_db_3" {
-    subnet_id      = aws_subnet.private-subnet-6.id
-    route_table_id = aws_route_table.private-route-table.id
+  subnet_id      = aws_subnet.private-subnet-6.id
+  route_table_id = aws_route_table.private-route-table.id
 }
