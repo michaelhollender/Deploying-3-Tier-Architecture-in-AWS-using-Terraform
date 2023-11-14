35d00ff7ed5eb0285d80a21d19ad20c8a3f44c95
mastersplinter2 <111327102+michaelhollender@users.noreply.github.com>
2023-11-13 23:41:13 -0500

    Changed 3 to Three

--- alb.tf
+++ alb.tf
@@ -24,7 +24,7 @@ resource "aws_lb_target_group" "alb_target_group" {
 
 resource "aws_lb_target_group_attachment" "web_attachment" {
     target_group_arn = aws_lb_target_group.alb_target_group.arn
-    target_id        = aws_instance.3TierWebLaunchTemplate.id
+    target_id        = aws_instance.ThreeTierWebLaunchTemplate.id
     port             = 80
 }
 
