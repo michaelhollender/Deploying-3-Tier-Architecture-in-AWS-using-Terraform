64f2b75c7a01914d446e095beba725a7804dd8b7
mastersplinter2 <111327102+michaelhollender@users.noreply.github.com>
2023-11-14 00:13:27 -0500

    Fixed Format

--- web_sg.tf
+++ web_sg.tf
@@ -3,50 +3,50 @@
 ##############################
 
 resource "aws_security_group" "webserver-security-group" {
-    name        = "Web Server Security Group"
-    description = "Enable http/https access on port 80/443 via ALB and ssh via ssh SG"
-    vpc_id      = aws_vpc.vpc_01.id
+  name        = "Web Server Security Group"
+  description = "Enable http/https access on port 80/443 via ALB and ssh via ssh SG"
+  vpc_id      = aws_vpc.vpc_01.id
 
-    ingress {
-        description     = "http access"
-        from_port       = 80
-        to_port         = 80
-        protocol        = "tcp"
-        security_groups = ["${aws_security_group.alb-security-group.id}"]
-    }
+  ingress {
+    description     = "http access"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = ["${aws_security_group.alb-security-group.id}"]
+  }
 
-    ingress {
-        description     = "https access"
-        from_port       = 443
-        to_port         = 443
-        protocol        = "tcp"
-        security_groups = ["${aws_security_group.alb-security-group.id}"]
-    }
+  ingress {
+    description     = "https access"
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    security_groups = ["${aws_security_group.alb-security-group.id}"]
+  }
 
-    ingress {
-        description     = "ssh access"
-        from_port       = 22
-        to_port         = 22
-        protocol        = "tcp"
-        security_groups = ["${aws_security_group.ssh-security-group.id}"]
-    }
+  ingress {
+    description     = "ssh access"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = ["${aws_security_group.ssh-security-group.id}"]
+  }
 
-    ingress {
-        description     = "icmp access"
-        from_port       = 8
-        to_port         = 0
-        protocol        = "icmp"
-        security_groups = ["${aws_security_group.ssh-security-group.id}"]
-    }
+  ingress {
+    description     = "icmp access"
+    from_port       = 8
+    to_port         = 0
+    protocol        = "icmp"
+    security_groups = ["${aws_security_group.ssh-security-group.id}"]
+  }
 
-    egress {
-        from_port       = 0
-        to_port         = 0
-        protocol        = "-1"
-        cidr_blocks     = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    tags = {
-        Name = "Web Server Security Group"
-    }
+  tags = {
+    Name = "Web Server Security Group"
+  }
 }
