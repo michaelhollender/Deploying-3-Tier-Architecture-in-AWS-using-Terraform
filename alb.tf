6c02c1b0c64a29c4e4c6c268df5fb36cf486c590
mastersplinter2 <111327102+michaelhollender@users.noreply.github.com>
2023-11-14 00:12:05 -0500

    Fixed Format

--- alb.tf
+++ alb.tf
@@ -3,46 +3,46 @@
 ###########################
 
 resource "aws_lb" "application-load-balancer" {
-    name                       = "web-external-load-balancer"
-    internal                   = false
-    load_balancer_type         = "application"
-    security_groups            = [aws_security_group.alb-security-group.id]
-    subnets                    = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id, aws_subnet.public-subnet-3.id]
-    enable_deletion_protection = false
+  name                       = "web-external-load-balancer"
+  internal                   = false
+  load_balancer_type         = "application"
+  security_groups            = [aws_security_group.alb-security-group.id]
+  subnets                    = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id, aws_subnet.public-subnet-3.id]
+  enable_deletion_protection = false
 
-    tags = {
-        Name = "App Load Balancer"
-    }
+  tags = {
+    Name = "App Load Balancer"
+  }
 }
 
 resource "aws_lb_target_group" "alb_target_group" {
-    name     = "appbalancertg"
-    port     = 80
-    protocol = "HTTP"
-    vpc_id   = aws_vpc.vpc_01.id
+  name     = "appbalancertg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc_01.id
 }
 
 resource "aws_lb_target_group_attachment" "web_attachment" {
-    target_group_arn = aws_lb_target_group.alb_target_group.arn
-    target_id        = aws_instance.ThreeTierWebLaunchTemplate.id
-    port             = 80
+  target_group_arn = aws_lb_target_group.alb_target_group.arn
+  target_id        = aws_instance.ThreeTierWebLaunchTemplate.id
+  port             = 80
 }
 
 # Create a listener on port 80 with redirect action
 resource "aws_lb_listener" "alb_http_listener" {
-    load_balancer_arn = aws_lb.application-load-balancer.arn
-    port              = 80
-    protocol          = "HTTP"
+  load_balancer_arn = aws_lb.application-load-balancer.arn
+  port              = 80
+  protocol          = "HTTP"
 
-    default action {
-        type = "redirect"
+  default "action" {
+    type = "redirect"
 
-        redirect {
-            port        = 443
-            protocol    = "HTTPS"
-            status_code = "HTTPS_301"
-        }
+    redirect {
+      port        = 443
+      protocol    = "HTTPS"
+      status_code = "HTTPS_301"
     }
+  }
 }
 
 
@@ -51,6 +51,6 @@ resource "aws_lb_listener" "alb_http_listener" {
 #######################
 
 output "lb_dns_name" {
-    description = "DNS name of the Load Balancer"
-    value       = "${aws_lb.application-load-balancer.dns_name}"
+  description = "DNS name of the Load Balancer"
+  value       = aws_lb.application-load-balancer.dns_name
 }
